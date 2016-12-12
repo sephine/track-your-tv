d0de47821c09f6584a188517e7e2920518356583
joannecdyer <joannecdyer@googlemail.com>
2016-12-12 09:06:12 -0800

    Fix for intro image file not loading properly in heroku.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,7 +6,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = 'aa798d3418e25829927d79602d92ae1c3ae2b70dea9e51662742c3748936ad23d1977769b629771ba67dc17b29437da6baa7c7e7d5670b7bd7829106fbcb6f55'
+  config.secret_key = ENV['DEVISE_SECRET_KEY'] if Rails.env.production?
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
