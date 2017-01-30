c94673cc695f2ed98e961a10215c01ade1367d49
joannecdyer <joannecdyer@googlemail.com>
2017-01-29 16:11:54 -0800

    automatic updates have not been happening due to a bug. Fix the bug and add rake task to get the ProgrammeInfos back up to date.

--- app/workers/update_programme_worker.rb
+++ app/workers/update_programme_worker.rb
@@ -3,7 +3,7 @@ class UpdateProgrammeWorker
   sidekiq_options queue: 'low'
 
   def perform(id)
-    programmeInfo = ProgrammeInfo.find(id: id)
+    programmeInfo = ProgrammeInfo.find(id)
     programmeInfo.update_from_tvdb  
   end
 end
---
+++ lib/tasks/update_db.rake
@@ -0,0 +1,6 @@
+task :update_database => :environment do
+  puts "Update all programmes..."
+  ProgrammeInfo.all.each do |programme_info|
+    UpdateProgrammeWorker.perform_async(programme_info.id)
+  end
+end
