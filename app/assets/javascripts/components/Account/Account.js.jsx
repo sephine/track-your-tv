3bdf0bdfa62c62171a5b580fad8fb44f64566b6a
joannecdyer <joannecdyer@googlemail.com>
2017-01-29 18:50:24 -0800

    add missing href=javascript:void(0); to links.

--- app/assets/javascripts/components/Account/Account.js.jsx
+++ app/assets/javascripts/components/Account/Account.js.jsx
@@ -22,7 +22,7 @@ var Account = React.createClass({
       <div>
         <h5>
           Personal Info
-          <a onClick={this.handleLinkClicked} id="update-personal-link" className="pull-right">
+          <a href="javascript:void(0);" onClick={this.handleLinkClicked} id="update-personal-link" className="pull-right">
             Update
           </a>
         </h5>
@@ -32,7 +32,7 @@ var Account = React.createClass({
 
         <h5>
           Email
-          <a onClick={this.handleLinkClicked} id="update-email-link" className="pull-right">
+          <a href="javascript:void(0);" onClick={this.handleLinkClicked} id="update-email-link" className="pull-right">
             Change
           </a>
         </h5>
@@ -42,7 +42,7 @@ var Account = React.createClass({
 
         <h5>
           Password
-          <a onClick={this.handleLinkClicked} id="update-password-link" className="pull-right">
+          <a href="javascript:void(0);" onClick={this.handleLinkClicked} id="update-password-link" className="pull-right">
             Change
           </a>
         </h5>
@@ -52,7 +52,7 @@ var Account = React.createClass({
 
         <h5>
           Cancel your account
-          <a onClick={this.props.onCancelClicked} id="cancel-link" className="pull-right">
+          <a href="javascript:void(0);" onClick={this.props.onCancelClicked} id="cancel-link" className="pull-right">
             Cancel
           </a>
         </h5>
